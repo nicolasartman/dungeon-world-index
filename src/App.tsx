020493993b9d147bede9857674bf6f4d8507a22b
Nick Artman <nicolasartman@gmail.com>
2021-10-15 23:31:06 -0700

    Build the whole app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { get as calculateEditDistance } from "fast-levenshtein"
+import { Box, Grommet, Heading, Text, TextInput } from "grommet"
+import minBy from "lodash/minBy"
+import { useMemo, useState } from "react"
+import "./App.css"
+import data from "./data"
+import theme from "./theme"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const getResults = (searchTerms: Array<string>) =>
+  searchTerms.map(
+    (searchTerm) =>
+      minBy(data, (datum) => calculateEditDistance(searchTerm, datum.name)) || {
+        name: "Not found",
+        description: `No result was found for ${searchTerm}`,
+        category: "",
+      },
+  )
+
+type ItemProps = {
+  name: string
+  description: string
+  category: string
 }
 
-export default App;
+const Item = ({ name, description, category }: ItemProps) => (
+  <Box
+    direction="column"
+    align="start"
+    pad={{ bottom: "40px" }}
+    width="768px"
+    style={{ maxWidth: "100%" }}
+  >
+    <Box direction="row" fill align="end" justify="between">
+      <Heading level="3" style={{ margin: "0" }}>
+        {name}
+      </Heading>
+      <Box>
+        <Text style={{ fontStyle: "italic", opacity: 0.7 }}>{category}</Text>
+      </Box>
+    </Box>
+    <Box height="5px" />
+    <Text textAlign="start">{description}</Text>
+  </Box>
+)
+
+function App() {
+  const [searchTerms, setSearchTerms] = useState<Array<string>>([])
+
+  const results = useMemo(
+    () => (searchTerms.length ? getResults(searchTerms) : data),
+    [searchTerms],
+  )
+
+  return (
+    <div className="App">
+      <Grommet full="min" theme={theme} themeMode="dark">
+        <Box fill pad={{ horizontal: "40px" }}>
+          <Box fill direction="row" justify="center">
+            <Heading level="1">Dungeon World Index</Heading>
+          </Box>
+          <Box direction="row" justify="center">
+            <Box direction="row" width="100%" style={{ maxWidth: "500px" }}>
+              <TextInput
+                onChange={({ target: { value } }) => {
+                  const terms = value
+                    .split(",")
+                    .map((term) => term.trim())
+                    .filter((term) => term)
+                  setSearchTerms(terms)
+                }}
+                placeholder="Enter term(s), separated by commas"
+              />
+            </Box>
+          </Box>
+          <Box height="40px" />
+          <Box direction="column" align="center" fill>
+            {results.map(({ name, description, category }) => (
+              <Item name={name} description={description} category={category} />
+            ))}
+          </Box>
+        </Box>
+      </Grommet>
+    </div>
+  )
+}
+
+export default App
---
+++ src/data.ts
@@ -0,0 +1,168 @@
+const data = [
+  {
+    name: "Applied",
+    description:
+      "It’s only useful when carefully applied to a person or to something they eat or drink.",
+    category: "General Equipment Tags",
+  },
+  {
+    name: "Awkward",
+    description: "It’s unwieldy and tough to use.",
+    category: "General Equipment Tags",
+  },
+  {
+    name: "+Bonus",
+    description:
+      "It modifies your effectiveness in a specified situation. It might be “+1 forward to spout lore” or “-1 ongoing to hack and slash.”",
+    category: "General Equipment Tags",
+  },
+  {
+    name: "n coins",
+    description:
+      "How much it costs to buy, normally. If the cost includes “-Charisma” a little negotiation subtracts the haggler’s Charisma score (not modifier) from the price.",
+    category: "General Equipment Tags",
+  },
+  {
+    name: "Dangerous",
+    description:
+      "It’s easy to get in trouble with it. If you interact with it without proper precautions the GM may freely invoke the consequences of your foolish actions.",
+    category: "General Equipment Tags",
+  },
+  { name: "Ration", description: "It’s edible, more or less.", category: "General Equipment Tags" },
+  {
+    name: "Requires",
+    description:
+      "It’s only useful to certain people. If you don’t meet the requirements it works poorly, if at all.",
+    category: "General Equipment Tags",
+  },
+  {
+    name: "Slow",
+    description: "It takes minutes or more to use.",
+    category: "General Equipment Tags",
+  },
+  {
+    name: "Touch",
+    description: "It’s used by touching it to the target’s skin.",
+    category: "General Equipment Tags",
+  },
+  {
+    name: "Two-handed",
+    description: "It takes two hands to use it effectively.",
+    category: "General Equipment Tags",
+  },
+  {
+    name: "n weight",
+    description:
+      "Count the listed amount against your load. Something with no listed weight isn’t designed to be carried. 100 coins in standard denominations is 1 weight. The same value in gems or fine art may be lighter or heavier.",
+    category: "General Equipment Tags",
+  },
+  {
+    name: "Worn",
+    description: "To use it, you have to be wearing it.",
+    category: "General Equipment Tags",
+  },
+  {
+    name: "n Uses",
+    description: "It can only be used n times.",
+    category: "General Equipment Tags",
+  },
+  {
+    name: "n Ammo",
+    description:
+      "It counts as ammunition for appropriate ranged weapons. The number indicated does not represent individual arrows or sling stones, but represents what you have left on hand.",
+    category: "Weapon Tags",
+  },
+
+  {
+    name: "Forceful",
+    description: "It can knock someone back a pace, maybe even off their feet.",
+    category: "Weapon Tags",
+  },
+
+  {
+    name: "+n Damage",
+    description:
+      "It is particularly harmful to your enemies. When you deal damage, you add n to it.",
+    category: "Weapon Tags",
+  },
+
+  {
+    name: "Ignores Armor",
+    description: "Don’t subtract armor from the damage taken.",
+    category: "Weapon Tags",
+  },
+
+  {
+    name: "Messy",
+    description:
+      "It does damage in a particularly destructive way, ripping people and things apart.",
+    category: "Weapon Tags",
+  },
+
+  {
+    name: "n Piercing",
+    description:
+      "It goes right through armor. When you deal damage with n piercing, you subtract n from the enemy’s armor for that attack.",
+    category: "Weapon Tags",
+  },
+
+  {
+    name: "Precise",
+    description:
+      "It rewards careful strikes. You use DEX to hack and slash with this weapon, not STR.",
+    category: "Weapon Tags",
+  },
+
+  {
+    name: "Reload",
+    description:
+      "After you attack with it, it takes more than a moment to reset for another attack.",
+    category: "Weapon Tags",
+  },
+
+  {
+    name: "Stun",
+    description: "When you attack with it, it does stun damage instead of normal damage.",
+    category: "Weapon Tags",
+  },
+
+  {
+    name: "Thrown",
+    description:
+      "Throw it at someone to hurt them. If you volley with this weapon, you can’t choose to mark off ammo on a 7–9; once you throw it, it’s gone until you can recover it.\nWeapons have tags to indicate the range at which they are useful. Dungeon World doesn’t inflict penalties or grant bonuses for “optimal range” or the like, but if your weapon says Hand and an enemy is ten yards away, a player would have a hard time justifying using that weapon against him.",
+    category: "Weapon Tags",
+  },
+
+  {
+    name: "Hand",
+    description: "It’s useful for attacking something within your reach, no further.",
+    category: "Weapon Tags",
+  },
+
+  {
+    name: "Close",
+    description: "It’s useful for attacking something at arm’s reach plus a foot or two.",
+    category: "Weapon Tags",
+  },
+
+  {
+    name: "Reach",
+    description:
+      "It’s useful for attacking something that’s several feet away—maybe as far as ten.",
+    category: "Weapon Tags",
+  },
+
+  {
+    name: "Near",
+    description: "It’s useful for attacking if you can see the whites of their eyes.",
+    category: "Weapon Tags",
+  },
+
+  {
+    name: "Far",
+    description: "It’s useful for attacking something in shouting distance.",
+    category: "Weapon Tags",
+  },
+]
+
+export default data
